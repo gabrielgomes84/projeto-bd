9b5815a4c9771f7925461f00c762f697a4312910
Gabriel Gomes <142534963+gabrielgomes84@users.noreply.github.com>
2025-04-15 22:55:27 -0300

    Create tarefa01-q17.sql

---
+++ tarefas/t01/tarefa01-q17.sql
@@ -0,0 +1,10 @@
+SELECT p.codigo, p.descricao
+FROM projeto p
+JOIN departamento d ON p.cod_depto = d.codigo
+JOIN funcionario f ON d.cod_gerente = f.codigo
+WHERE f.salario > ALL (
+  SELECT f2.salario
+  FROM departamento d2
+  JOIN funcionario f2 ON d2.cod_gerente = f2.codigo
+  WHERE d2.codigo <> d.codigo
+);
