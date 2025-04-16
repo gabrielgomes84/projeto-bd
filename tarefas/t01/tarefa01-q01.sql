4f353ea4cb6fa5007eef2abb1222b258491135ad
Gabriel Gomes <142534963+gabrielgomes84@users.noreply.github.com>
2025-04-15 23:13:19 -0300

    Update tarefa01-q01.sql #1
    esqueci do id D:

--- tarefas/t01/tarefa01-q01.sql
+++ tarefas/t01/tarefa01-q01.sql
@@ -1,4 +1,4 @@
--- consulta que seleciona os nomes dos funcionários que recebem salários superiores à média salarial do departamento 2
+-- consulta que seleciona os nomes dos funcionários que recebem salários superiores à média salarial do departamento 2 QUESTÃO 1 DA LISTA
 SELECT f.nome
 FROM funcionario f
 WHERE f.salario > (SELECT AVG(f2.salario) 
