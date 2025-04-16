76a60e88498fb55c4c08467299be5063387286a8
Gabriel Gomes <142534963+gabrielgomes84@users.noreply.github.com>
2025-04-15 22:24:33 -0300

    cria uma consulta questao 1

---
+++ tarefas/t01/tarefa01-q01.sql
@@ -0,0 +1,6 @@
+-- consulta que seleciona os nomes dos funcionários que recebem salários superiores à média salarial do departamento 2
+SELECT f.nome
+FROM funcionario f
+WHERE f.salario > (SELECT AVG(f2.salario) 
+                    FROM funcionario f2 
+                    WHERE f2.cod_depto = 2);
